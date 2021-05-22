df6b0e49236835d3cef2a76d687fbe6d3ad268fa
KimHyungkeun <hyungkeun.kim95@gmail.com>
2021-05-22 17:52:08 +0900

    SpringProject

--- ModelAndDB/src/main/java/com/example/modelanddb/vo/MyData.java
+++ ModelAndDB/src/main/java/com/example/modelanddb/vo/MyData.java
@@ -10,12 +10,13 @@ import javax.persistence.*;
 @Setter
 @AllArgsConstructor
 
-@Entity
+// id:메인키로 사용되는 번호, name:이름, mail:메일주소, age:나이, memo:메모
+@Entity //엔터티 클래스라는것을 나타내는 에너테이션
 @Table(name="mydata")
 public class MyData {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id //해당 컬럼을 메인키로 지정
+    @GeneratedValue(strategy = GenerationType.AUTO) //값을 자동 생성하며, strategy를 통해 생성 방식을 지정
     @Column
     private long id;
 
