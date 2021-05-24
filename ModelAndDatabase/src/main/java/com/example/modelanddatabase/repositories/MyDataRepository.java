4b678464a1d8c2fe7bd37e1894b5f0e4dc2ec822
KimHyungkeun <hyungkeun.kim95@gmail.com>
2021-05-24 21:34:42 +0900

    210524

--- ModelAndDatabase/src/main/java/com/example/modelanddatabase/repositories/MyDataRepository.java
+++ ModelAndDatabase/src/main/java/com/example/modelanddatabase/repositories/MyDataRepository.java
@@ -6,11 +6,17 @@ import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 
 import javax.persistence.Entity;
+import java.util.List;
 
 
 @Repository
 public interface MyDataRepository extends JpaRepository<MyData, Long>{
     // ID번호를 인수로 하여 MyData 인스턴스를 추출한다
     // 책에서는 findById라 나와있으나, JpaRepository를 이용한다면 getById가 맞다
+
     public MyData getById(Long name);
+    public List<MyData> findByNameLike(String name);
+    public List<MyData> findByIdIsNotNullOrderByIdDesc();
+    public List<MyData> findByAgeGreaterThan(Integer age);
+    public List<MyData> findByAgeBetween(Integer age1, Integer age2);
 }
