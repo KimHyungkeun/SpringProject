c66b6f5d9f636634519dbcdd4d556dc0e2c8abd2
KimHyungkeun <hyungkeun.kim95@gmail.com>
2021-05-24 23:15:01 +0900

    SpringProgject

--- ModelAndDatabase/src/main/java/com/example/modelanddatabase/vo/MyData.java
+++ ModelAndDatabase/src/main/java/com/example/modelanddatabase/vo/MyData.java
@@ -15,7 +15,7 @@ import javax.validation.constraints.NotEmpty;
 @Setter
 
 // id:메인키로 사용되는 번호, name:이름, mail:메일주소, age:나이, memo:메모
-@Entity //엔터티 클래스라는것을 나타내는 에너테이션
+@Entity //엔터티 클래스라는것을 나타내는 annotation
 @Table(name="mydata")
 public class MyData {
 
